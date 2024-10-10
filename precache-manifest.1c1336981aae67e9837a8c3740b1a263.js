385425f314bec47c81370940366aa644540812b2
Rodion <redia1994@mail.ru>
2024-10-10 15:41:32 +0300

    Updates

---
+++ precache-manifest.1c1336981aae67e9837a8c3740b1a263.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f461bad0d351b9c0713ea6cf8bfa2b",
+    "url": "/proectMessenger/index.html"
+  },
+  {
+    "revision": "f8dfa6db23e8e15dd79a",
+    "url": "/proectMessenger/static/css/3.a00ea10b.chunk.css"
+  },
+  {
+    "revision": "68f6cac528d54cdaf270",
+    "url": "/proectMessenger/static/css/4.86f51430.chunk.css"
+  },
+  {
+    "revision": "57f5fb01a14015818ddf",
+    "url": "/proectMessenger/static/css/main.974e809d.chunk.css"
+  },
+  {
+    "revision": "b929be5afe27756706cc",
+    "url": "/proectMessenger/static/js/2.b871545d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/proectMessenger/static/js/2.b871545d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8dfa6db23e8e15dd79a",
+    "url": "/proectMessenger/static/js/3.9b28aa64.chunk.js"
+  },
+  {
+    "revision": "68f6cac528d54cdaf270",
+    "url": "/proectMessenger/static/js/4.0da1e356.chunk.js"
+  },
+  {
+    "revision": "57f5fb01a14015818ddf",
+    "url": "/proectMessenger/static/js/main.f409cd3b.chunk.js"
+  },
+  {
+    "revision": "f497b0b965343307d16b",
+    "url": "/proectMessenger/static/js/runtime-main.eae1d430.js"
+  },
+  {
+    "revision": "b6ad69d526000de7b4f583eee5441fa2",
+    "url": "/proectMessenger/static/media/preloader.b6ad69d5.svg"
+  },
+  {
+    "revision": "856e78458266b60c4b7b4856d21ec7d5",
+    "url": "/proectMessenger/static/media/user.856e7845.png"
+  }
+]);
